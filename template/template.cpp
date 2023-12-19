a327673f6b7f7f2c428a836369cd0845f5b53cdc
Khushboo Agarwal <kushbooagarwal3584@gmail.com>
2023-12-19 14:03:26 +0530

    template modified

--- template/template.cpp
+++ template/template.cpp
@@ -1,5 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long 
+#define for_l for(int i=0;i<n;i++)
+#define stack stack<int>
+#define mpv unordered_map<int, vector<int>>
+#define vv vector<vector<int>>
+#define pii pair<int, int>
 
 int main(){
     ios_base::sync_with_stdio(false);
